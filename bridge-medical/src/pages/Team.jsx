06c260b933cb1582416609171d5725d5a08cb833
Finn <52647274+frodger24@users.noreply.github.com>
2025-02-14 21:42:53 +1100

    added features and designs on the services page

--- bridge-medical/src/pages/Team.jsx
+++ bridge-medical/src/pages/Team.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import "./Home.css";
 import Layout from "./Styling/Layout";
 import "./Team.css";
+import { FaLongArrowAltRight, FaExpandAlt } from "react-icons/fa";
 
 const Team = () => {
   return (
@@ -12,28 +13,46 @@ const Team = () => {
         <div className="team-float">
           <div className="job-tile">
             <img
+              src="src\assets\images\malcolm-profile.jpg"
+              className="team-pic"
+            ></img>
+            <h2 className="name-title">Malcolm Kanter, MD</h2>
+            <h1 className="job-title">CEO/Founder</h1>
+            <FaLongArrowAltRight className="arrow-link" />
+          </div>
+          <div className="job-tile">
+            <img
+              src="src\assets\images\anthony-profile.jpg"
+              className="team-pic"
+            ></img>
+            <h2 className="name-title">Anthony Trasmundi</h2>
+            <h1 className="job-title">Chief Operating Officer</h1>
+            <FaLongArrowAltRight className="arrow-link" />
+          </div>
+          <div className="job-tile">
+            <img
               src="src\assets\images\jacob-profile.jpg"
               className="team-pic"
             ></img>
-            <h1 className="job-title">CEO/Founder</h1>
-            <h2 className="name-title">Malcolm J. Kanter, MD</h2>
+            <h2 className="name-title">Jacob</h2>
+            <h1 className="job-title">Director of Behavioral Health</h1>
+            <FaLongArrowAltRight className="arrow-link" />
           </div>
           <div className="job-tile">
-            <img className="team-pic"></img>
-            <h1 className="job-title">CEO/Founder</h1>
-            <h2 className="name-title">Malcolm J. Kanter, MD</h2>
-          </div>
-          <div className="job-tile">
-            <img className="team-pic"></img>
-            <h1 className="job-title">CEO/Founder</h1>
-            <h2 className="name-title">Malcolm J. Kanter, MD</h2>
-          </div>
-          <div className="job-tile">
-            <img className="team-pic"></img>
-            <h1 className="job-title">CEO/Founder</h1>
-            <h2 className="name-title">Malcolm J. Kanter, MD</h2>
+            <img
+              src="src\assets\images\spiegelman-profile.jpg"
+              className="team-pic"
+            ></img>
+            <h2 className="name-title">Dr. Spigelman</h2>
+            <h1 className="job-title">
+              Clinical Psychologist / Neuro Psychologist
+            </h1>
+            <FaLongArrowAltRight className="arrow-link" />
           </div>
         </div>
+      </div>
+      <div className="provider-section">
+        <h1 className="team-title">Our Providers</h1>
       </div>
     </Layout>
   );
