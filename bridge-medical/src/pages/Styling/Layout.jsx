a69559d620541cb8ae00f363c0d4bef871e3af2d
Finn <52647274+frodger24@users.noreply.github.com>
2025-02-14 12:26:41 +1100

    Fixed the footer somewhat, needs fixing on services page

--- bridge-medical/src/pages/Styling/Layout.jsx
+++ bridge-medical/src/pages/Styling/Layout.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
+import "./Layout.css";
 
 const Layout = ({ children }) => {
   return (
-    <>
+    <div className="layout-container">
       <Header />
-      {children}
+      <div className="page-content">{children}</div>
       <Footer />
-    </>
+    </div>
   );
 };
 
--- bridge-medical/src/pages/Team.jsx
+++ bridge-medical/src/pages/Team.jsx
@@ -2,12 +2,38 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./Home.css";
 import Layout from "./Styling/Layout";
+import "./Team.css";
 
 const Team = () => {
   return (
     <Layout>
-      <div>
-        <h1>Hello, Team page!</h1>
+      <div className="team-section">
+        <h1 className="team-title">Our Team</h1>
+        <div className="team-float">
+          <div className="job-tile">
+            <img
+              src="src\assets\images\jacob-profile.jpg"
+              className="team-pic"
+            ></img>
+            <h1 className="job-title">CEO/Founder</h1>
+            <h2 className="name-title">Malcolm J. Kanter, MD</h2>
+          </div>
+          <div className="job-tile">
+            <img className="team-pic"></img>
+            <h1 className="job-title">CEO/Founder</h1>
+            <h2 className="name-title">Malcolm J. Kanter, MD</h2>
+          </div>
+          <div className="job-tile">
+            <img className="team-pic"></img>
+            <h1 className="job-title">CEO/Founder</h1>
+            <h2 className="name-title">Malcolm J. Kanter, MD</h2>
+          </div>
+          <div className="job-tile">
+            <img className="team-pic"></img>
+            <h1 className="job-title">CEO/Founder</h1>
+            <h2 className="name-title">Malcolm J. Kanter, MD</h2>
+          </div>
+        </div>
       </div>
     </Layout>
   );
