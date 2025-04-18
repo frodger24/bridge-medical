a51d2c9456fbb0913ab7e0361ddf1930eb0b5405
Finn <52647274+frodger24@users.noreply.github.com>
2025-04-18 17:07:11 +1000

    Deploy updated site

--- src/pages/Index.jsx
+++
@@ -1,6 +0,0 @@
-export { default as Home } from "./Home";
-export { default as Team } from "./Team";
-export { default as Services } from "./Services";
-export { default as Jobs } from "./Jobs";
-export { default as Referral } from "./Referral";
-export { default as Providers } from "./Providers";
